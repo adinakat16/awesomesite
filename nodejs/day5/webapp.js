3b812a65eee73c2b3d4145982dea26699f975552
aditya <adi.nakat16@gmail.com>
2021-12-22 19:17:17 +0530

    mo

--- nodejs/day5/webapp.js
+++ nodejs/day5/webapp.js
@@ -1,9 +1,33 @@
 //Nodejs is used to create nodejs Application and host it
 
-const http=require('http');
-const fs= require('fs');
+const http = require('http');
+const fs = require('fs');
+const { url } = require('inspector');
 
-http.createServer((req,res)=>{
-console.log(req);
+function display(url, res) {
+    const file = __dirname + url + ".html";//CurrentDir + fileName + extension will give the actual filename of UR project.
+    fs.createReadStream(file).pipe(res)
+}
 
+http.createServer((req, res) => {
+if (req.url != '/favicon.ico') {
+       
+        console.log(req.url);
+        switch (req.url) {
+            case '/homepage':
+                display(req.url, res);
+                break;
+            case '/registration':
+                display(req.url, res);
+                break;
+            case '/contactus':
+                display(req.url, res);
+                break;
+            case '/aboutus':
+                display(req.url, res);
+                break;
+        }
+        
+        
+    }
 }).listen(1234)
