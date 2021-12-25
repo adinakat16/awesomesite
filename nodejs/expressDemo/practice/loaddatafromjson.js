9b92c741e7bbc06fc338abf82106a240791c3973
aditya <adi.nakat16@gmail.com>
2021-12-25 09:54:34 +0530

    getmthod.done()

--- nodejs/expressDemo/practice/loaddatafromjson.js
+++ nodejs/expressDemo/practice/loaddatafromjson.js
@@ -13,20 +13,20 @@ app.get('/emp',(req,res)=>{
     })
 
 })
-// app.get('/empname',(req,res)=>{
+app.get('/empname',(req,res)=>{
     
-//     var myurl="localhost:1234"+req.url;
-//     //console.log(myurl)
-//     var qdata=url.parse(myurl,true).query;
-//     //console.log(qdata.name)
-//     var qdatatofind=qdata.name;
-//     fs.readFile('empjson.json',(err,data)=>{
-//         data=JSON.parse(data);
-//         data=data.filter((e)=>e.first_name.includes(qdatatofind));
-//          console.log(data);
-//         res.send(data);
-//     })
-// })
+    var myurl="localhost:1234"+req.url;
+    //console.log(myurl)
+    var qdata=url.parse(myurl,true).query;
+    //console.log(qdata.name)
+    var qdatatofind=qdata.name;
+    fs.readFile('empjson.json',(err,data)=>{
+        data=JSON.parse(data);
+        data=data.filter((e)=>e.first_name.includes(qdatatofind));
+         console.log(data);
+        res.send(data);
+    })
+})
 
 app.listen(1234,()=>{
     console.log("server is running on 1234 port......")
